036bc95926e63850acb5921bd2259c1bd3cf33a1
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-02-24 01:47:57 +0100

    Update docblock

--- src/Black/Component/ContactPoint/Domain/Model/ContactPoint.php
+++ src/Black/Component/ContactPoint/Domain/Model/ContactPoint.php
@@ -5,22 +5,55 @@ namespace Black\Component\ContactPoint\Domain\Model;
 use Address\PostalAddress as Address;
 use Black\DDD\DDDinPHP\Domain\Model\Entity;
 
+/**
+ * Class ContactPoint
+ */
 class ContactPoint implements Entity
 {
+    /**
+     * @var ContactPointId
+     */
     protected $contactPointId;
 
+    /**
+     * @var
+     */
     protected $contactType;
 
+    /**
+     * @var
+     */
     protected $telephone;
 
+    /**
+     * @var
+     */
     protected $mobile;
 
+    /**
+     * @var
+     */
     protected $email;
 
+    /**
+     * @var
+     */
     protected $faxNumber;
 
+    /**
+     * @var Address
+     */
     protected $address;
 
+    /**
+     * @param ContactPointId $contactPointId
+     * @param $contactType
+     * @param $telephone
+     * @param $mobile
+     * @param $email
+     * @param $faxNumber
+     * @param Address $address
+     */
     public function __construct(
         ContactPointId $contactPointId,
         $contactType,
@@ -39,31 +72,49 @@ class ContactPoint implements Entity
         $this->address        = $address;
     }
 
+    /**
+     * @return ContactPointId
+     */
     public function getContactPointId()
     {
         return $this->contactPointId;
     }
 
+    /**
+     * @return mixed
+     */
     public function getContactType()
     {
         return $this->contactType;
     }
 
+    /**
+     * @return mixed
+     */
     public function getTelephone()
     {
         return $this->telephone;
     }
 
+    /**
+     * @return mixed
+     */
     public function getMobile()
     {
         return $this->mobile;
     }
 
+    /**
+     * @return mixed
+     */
     public function getFaxNumber()
     {
         return $this->faxNumber;
     }
 
+    /**
+     * @return Address
+     */
     public function getAddress()
     {
         return $this->address;
